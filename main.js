7582674ce50432c731a0f930c6d4a3585d2f2b2a
stacey.tadrus@gmail.com <stacey.tadrus@gmail.com>
2025-03-16 18:35:33 -0500

    .length practice

---
+++ main.js
@@ -0,0 +1,16 @@
+// Starter Code (Pre-filled):
+// Predefined strings
+let stringOne = "Coding Bootcamp";
+let stringTwo = "JavaScript";
+let stringThree = "";
+
+// Your task: Assign the length of each string to the corresponding variable
+let lengthOne = stringOne;
+let lengthTwo = stringTwo;
+let lengthThree = stringThree;
+
+// Your code here
+
+console.log(lengthOne, "has ", lengthOne.length);
+console.log(lengthTwo, "has ", lengthTwo.length);
+console.log("lengthThree ", lengthThree, "has ", lengthThree.length);
